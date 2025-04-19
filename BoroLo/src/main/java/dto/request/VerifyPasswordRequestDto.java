76eb686b842b4767f51bbbc5aacb3f549bbe705d
Ubokchi <105459234+Ubokchi@users.noreply.github.com>
2025-04-19 19:32:18 +0900

    필드 및 getter setter 추

--- BoroLo/src/main/java/dto/request/VerifyPasswordRequestDto.java
+++ BoroLo/src/main/java/dto/request/VerifyPasswordRequestDto.java
@@ -1,12 +1,12 @@
 package dto.request;
 
 public class VerifyPasswordRequestDto {
+    private Integer user_id;
     private String password;
 
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
-} 
+    public Integer getUser_id() { return user_id; }
+    public void setUser_id(Integer user_id) { this.user_id = user_id; }
+
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
+}
